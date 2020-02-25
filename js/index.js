f787e62710ffa2658f6d0099441cfcc39f100c25
briantical <lutbrianivan@gmail.com>
2020-02-25 18:37:50 +0300

    Building the forms

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//Define the numerous user roles
+const SUPERVISOR = "supervisor";
+const SALES = "sales";
+const MANAGER = "manager";
+const CUSTOMER = "customer";
+
+const ROLES = [SUPERVISOR, SALES, MANAGER, CUSTOMER];
+
+//Define the load details for each vehicle types
+const LOAD_DETAILS = [
+  {
+    vehicle: "bodaboda",
+    down_payment: 850000,
+    payable_duration: 2,
+    monthly_installment: 214590
+  },
+  {
+    vehicle: "tuktuk",
+    down_payment: 950000,
+    payable_duration: 2,
+    monthly_installment: 294590
+  }
+];
